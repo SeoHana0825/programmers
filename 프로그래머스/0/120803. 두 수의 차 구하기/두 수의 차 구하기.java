3256515bb924ed8c3c244fd11b7e3ca493ff1977
SEO HANA <onnuri1226@gmail.com>
2026-02-24 09:21:52 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer ;
+        
+        if (-5000 <= num1 && 5000>= num2) {
+            if (-5000 <= num2 && 5000>=num2);
+        }
+        
+        return num1 - num2;
+    }
+}
